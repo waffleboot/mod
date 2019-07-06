b612e778ad0cd40a0c56affdd17ab88bce7ae6e2
Andrei Yangabishev <waffleboot@gmail.com>
2019-07-06 15:56:47 +0300

    master

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/waffleboot/mod/internal/app/myapp"
 	"github.com/waffleboot/mod/internal/pkg/myprivlib"
-	"github.com/waffleboot/mod/pkg/mypubliclib"
+	lib "github.com/waffleboot/mod/pkg/mypubliclib"
+	lib2 "github.com/waffleboot/mod/v2/pkg/mypubliclib"
 )
 
 func main() {
 	myapp.Foo()
 	myprivlib.Foo()
-	mypubliclib.Foo()
+	lib.Foo()
+	lib2.Foo(1)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/waffleboot/mod
 
 go 1.12
+
+require github.com/waffleboot/mod/v2 v2.0.5
